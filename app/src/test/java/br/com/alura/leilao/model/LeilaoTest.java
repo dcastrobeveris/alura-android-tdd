e66589d6a6842ade2b4b2c22dad7aff02636484b
Diego Barcellos <dcastrob@everis.com>
2021-07-09 17:06:44 -0300

    Testando a feature para maior lance

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class LeilaoTest extends TestCase {
 
     @Test
-    public void testGetDescricao() {
+    public void testgetDescricao() {
         // Criar cenário de teste
         Leilao console = new Leilao("Console");
 
@@ -17,4 +17,18 @@ public class LeilaoTest extends TestCase {
         // Testar resultado esperado
         assertEquals("Console", descricaoDevolvida);
     }
+
+    @Test
+    public void testgetMaiorLance(){
+        Leilao console = new Leilao("Console");
+        console.propoe(new Lance(new Usuario("Diego"), 200.0));
+
+        // Delta = a diferença entre os valores com ponto flutuante e se ele for maior,
+        // significa que os valores são equivalentes.
+
+        double maiorLanceDevolvido = console.getMaiorLance();
+
+        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+    }
+
 }
