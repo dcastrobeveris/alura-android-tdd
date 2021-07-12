852609d9a93ddf0b6bbc8b72bf01c166a70bd778
Diego Barcellos <dcastrob@everis.com>
2021-07-12 16:21:38 -0300

    Invalidando testes

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -48,16 +48,6 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolveMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemDecrescente(){
-        CONSOLE.propoe(new Lance(DIEGO, 10000.0));
-        CONSOLE.propoe(new Lance(new Usuario("Ana"), 9000.0));
-
-        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
-
-        assertEquals(10000.0, maiorLanceDevolvido, DELTA);
-    }
-
-    @Test
     public void deve_DevolveMenorLance_QuandoRecebeApenasUmLance(){
         CONSOLE.propoe(new Lance(DIEGO, 200.0));
 
@@ -77,16 +67,6 @@ public class LeilaoTest {
         double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(100.0, menorLanceDevolvido, DELTA);
-    }
-
-    @Test
-    public void deve_DevolveMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemDecrescente(){
-        CONSOLE.propoe(new Lance(DIEGO, 10000.0));
-        CONSOLE.propoe(new Lance(new Usuario("Ana"), 9000.0));
-
-        double menorLanceDevolvido = CONSOLE.getMenorLance();
-
-        assertEquals(9000.0, menorLanceDevolvido, DELTA);
     }
 
     @Test
@@ -176,6 +156,16 @@ public class LeilaoTest {
         double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(0.0, menorLanceDevolvido, DELTA);
+    }
+
+    @Test
+    public void naoDeve_AdicionarLance_QuandoForMenorQueOMaiorLance(){
+        CONSOLE.propoe(new Lance(DIEGO, 500.0));
+        CONSOLE.propoe(new Lance(new Usuario("Ana"), 400.0));
+
+        int quantidadeLancesDevolvida = CONSOLE.quantidadeLances();
+
+        assertEquals(1, quantidadeLancesDevolvida);
     }
 
 }
